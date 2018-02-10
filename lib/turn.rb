fda3b556958cd19606440c2e154ed13fbb891d09
Rima Jean <rima.jean@gmail.com>
2018-02-10 23:37:06 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,8 +12,7 @@ end
 
 def valid_move?(board, position)
   index = position.to_i-1
-  index.between(0,8)
-  position_taken?(board, index) == false
+  index.between(0,8) && !position_taken?(board, index)
 end
 
 def position_taken?(board, index)
