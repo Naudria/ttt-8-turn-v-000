3f97c3b24da77d7a727f2adeeca6f991cdde1989
Rima Jean <rima.jean@gmail.com>
2018-02-11 00:58:33 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -20,7 +20,7 @@ def position_taken?(board, index)
 end
 
 def move(board, position, token)
-  index = position.to_i
+  index = position.to_i 
   board[index] = token
   display_board(board)
 end
@@ -35,4 +35,16 @@ def turn(board)
   else
     turn(board)
 end
+end
+
+def turn_count(board)
+  9 - board.count(" ")
+end
+
+def current_player(board)
+  if turn_count(board).even?
+    "X"
+  else
+    "O"
+  end
 end
