cb5013c2e7e43f827ee2327cd6ed14f6c0818568
Rima Jean <rima.jean@gmail.com>
2018-02-11 00:38:45 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -27,7 +27,7 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
-  position = gets.strip
+  position.to_i = gets.strip
   if valid_move?(board, position)
     token = current_player(board)
     move(board, position, token)
